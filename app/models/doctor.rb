884a5ea9ebff0bf0307773c923ac409554accde7
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-04-24 11:28:31 -0400

    create doctor instance methods

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,12 @@ class Doctor < ApplicationRecord
   belongs_to :hospital
   has_many :doctor_patients
   has_many :patients, through: :doctor_patients
+
+  def find_hospital
+    hospital.name
+  end
+
+  def list_patients
+    patients.pluck(:name).join(', ')
+  end
 end
